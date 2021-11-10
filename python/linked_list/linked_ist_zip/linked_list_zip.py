e8bee5de290b74232ec2e9e1f20d655b4217b612
rawnaq <engrawnaqaburummn@gmail.com>
2021-11-10 09:39:45 +0200

    not final

--- python/linked_list/linked_ist_zip/linked_list_zip.py
+++ python/linked_list/linked_ist_zip/linked_list_zip.py
@@ -50,27 +50,39 @@ class LinkedList:
             current.next = node
 
 #==============================Ziplist=========================================
-def zipLists(self,ll1, ll2):
-    current1 = ll1.head
-    current2 = ll2.head
+    def zipLists(ll1,ll2):
+
+        current1 = ll1.head
+        current2 = ll2.head
+
+        if current1 == None or current2 == None:
+            if current1:
+                return ll1
+            elif current2:
+                return ll2
+            else:
+             return "Both of the linked list is empty"
 
 
-    while current1 != None and  current2 != None and self.head != None :
-        current1=current1.next
-        current2=current2.next
-
-        current1.next=current2
-        current2.next=current1
 
 
-        current1=current1.next
-        current2=current2.next
-        ll2.head=ll1.head
+        li = []
+        while current1 or current2:
+            if(current1):
+                li+=[current1.value]
+                current1 = current1.next
+            if(current2):
+                li+=[current2.value]
+                current2 = current2.next
+            print(li)
+        nodee='head -> '
+        for i in li:
+         nodee+="{%s} -> "%(i)
+        nodee+='None'
+
+        return nodee
 
 
-    while self.head != None:
-            yield self.head.data
-            self.head = self.head.next
 
 
 
@@ -79,13 +91,12 @@ def zipLists(self,ll1, ll2):
 
 if __name__ == "__main__":
     ll1 =LinkedList()
-    ll1.append(1)
-    ll1.append(3)
-    ll1.append(5)
+  
     ll2 = LinkedList()
     ll2.append(2)
     ll2.append(4)
     ll2.append(6)
     print(ll1)
     print(ll2)
-    print(zipLists(ll1, ll2))
+    print(LinkedList.zipLists(ll1,ll2))
+
