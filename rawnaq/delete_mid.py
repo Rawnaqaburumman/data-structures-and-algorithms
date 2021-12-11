d42ca313bd3f0594a153c82dd4073ca739535ffd
rawnaq <engrawnaqaburummn@gmail.com>
2021-12-11 23:14:55 +0200

    initial

---
+++ rawnaq/delete_mid.py
@@ -0,0 +1,21 @@
+
+class Node:
+
+	# Function to initialise the node object
+	def __init__(self, data):
+		self.data = data # Assign data
+		self.next = None # Initialize next as null
+
+
+# Constructor to initialize the node object
+class LinkedList:
+
+	# Function to initialize head
+	def __init__(self):
+		self.head = None
+
+ 
+
+
+
+
